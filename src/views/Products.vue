12812df89e4c83e5c9e04144fc62e801f0fbd767
Barthezzone1 <s219117@student.uek.krakow.pl>
2024-05-08 21:26:56 +0200

    test

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -22,24 +22,11 @@
           </div>
         </div>
       </router-link>
-      <router-link v-for="product in filteredProducts" :key="product.id" :to="{ name: 'ProductDetails', params: { id: product.id } }">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg border-black m-4">
-          <div class="px-6 py-4 flex flex-col items-center">
-            <div class="font-bold text-xl mb-2 text-center">{{ product.id }}</div>
-            <img :src="product.imageURL" alt="Product Image" class="w-24 h-24 mb-4">
-            <p class="text-gray-700 text-base text-center">
-              Grams: {{ product.grams }}g <br>
-              Kcal: {{ product.kcal }} <br>
-            </p>
-          </div>
-        </div>
-      </router-link>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
 import { ref, onMounted } from "vue";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '../main.js';
@@ -52,16 +39,6 @@ onMounted(async () => {
   const querySnapshot = await getDocs(collection(db, "products"));
   let allProducts = []
   querySnapshot.forEach((doc) => {
-    const prod = {
-      id: doc.id,
-      carbs: doc.data().carbs,
-      fat: doc.data().fat,
-      grams: doc.data().grams,
-      kcal: doc.data().kcal,
-      protein: doc.data().protein,
-      imageURL: doc.data().imageURL
-    }
-    allProducts.push(prod);
     const prod = {
       id: doc.id,
       carbs: doc.data().carbs,
